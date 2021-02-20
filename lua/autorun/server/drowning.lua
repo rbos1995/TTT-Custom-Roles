bac0e753d231092e6c70a6ad554fa715b4af8626
Malivil <mlebson@outlook.com>
2021-02-20 17:57:17 -0500

    Minor cleanup

--- lua/autorun/server/drowning.lua
+++ lua/autorun/server/drowning.lua
@@ -1,15 +1,12 @@
-hook.Add("EntityTakeDamage", "swaggertyes", function(ent, info)
+hook.Add("EntityTakeDamage", "HandleDrowningDamage", function(ent, info)
     if (info:GetDamageType() == DMG_DROWN and ent:IsPlayer()) then
-        local health = ent:Health()
-        local health = health - info:GetDamage()
-        if (health <= 0) then
+        local health = ent:Health() - info:GetDamage()
+        if health <= 0 then
             ent.DiedByWater = true
         end
     end
 end)
 
-hook.Add("PlayerSpawn", "whatthefuckamIdoing", function(ply)
-    if ply.DiedByWater or false then
-        ply.DiedByWater = false
-    end
+hook.Add("PlayerSpawn", "ResetDrownDeath", function(ply)
+    ply.DiedByWater = false
 end)
